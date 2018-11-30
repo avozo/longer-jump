2b64285602ccd842f13226a850b2156d1c3c1dfd
Zelly D. Snyder <zelly@iappp.app>
2018-11-30 10:33:42 -0500

    pulse highlights line momentarily upon navigation for a nice visual effect

--- hst-mode.el
+++ hst-mode.el
@@ -126,7 +126,9 @@
 					)))
 
 		;; actually move the cursor
-		(goto-char (elt history dest-history-idx))
+		(let ((destination-char (elt history dest-history-idx)))
+          (goto-char destination-char)
+          (pulse-momentary-highlight-one-line destination-char))
         ;; also move window to middle of screen
         (recenter nil)))))
 
